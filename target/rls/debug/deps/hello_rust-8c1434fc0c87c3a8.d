d517fdc4a23f452ab4c10f0a3f3681b15d71afed
Matías Barrios <matias.barriosn@gmail.com>
2020-04-19 01:32:21 -0400

    fix: target ignored

--- target/rls/debug/deps/hello_rust-8c1434fc0c87c3a8.d
+++
@@ -1,5 +0,0 @@
-/Users/matiasbn/rust-projects/hello_rust/target/rls/debug/deps/hello_rust-8c1434fc0c87c3a8.rmeta: src/main.rs
-
-/Users/matiasbn/rust-projects/hello_rust/target/rls/debug/deps/hello_rust-8c1434fc0c87c3a8.d: src/main.rs
-
-src/main.rs:
--- target/rls/debug/deps/hello_rust-acb9dd2e9d862b40.d
+++
@@ -1,5 +0,0 @@
-/Users/matiasbn/rust-projects/hello_rust/target/rls/debug/deps/hello_rust-acb9dd2e9d862b40.rmeta: src/main.rs
-
-/Users/matiasbn/rust-projects/hello_rust/target/rls/debug/deps/hello_rust-acb9dd2e9d862b40.d: src/main.rs
-
-src/main.rs:
