899e27a7808a026ba997a69d2fd6cc4c22b9a5f0
Matías Barrios <matias.barriosn@gmail.com>
2020-04-19 01:27:27 -0400

    feat: things

---
+++ target/rls/debug/deps/hello_rust-acb9dd2e9d862b40.d
@@ -0,0 +1,5 @@
+/Users/matiasbn/rust-projects/hello_rust/target/rls/debug/deps/hello_rust-acb9dd2e9d862b40.rmeta: src/main.rs
+
+/Users/matiasbn/rust-projects/hello_rust/target/rls/debug/deps/hello_rust-acb9dd2e9d862b40.d: src/main.rs
+
+src/main.rs:
